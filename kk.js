4720c2c3018005f963b004afaad15b7ce6a3bfc0
AbdallahDaher <abdallahdaher785@gmail.com>
2024-10-13 16:59:53 +0300

    any

---
+++ kk.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000; // You can change this to any port number you prefer
+
+// Define a route that returns "Hello, World!"
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+// Start the server and listen on the specified port
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
