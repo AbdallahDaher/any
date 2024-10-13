690c271f8fdb978f0d563199f66764e119db9c23
AbdallahDaher <abdallahdaher785@gmail.com>
2024-10-13 17:35:24 +0300

    any

--- kk.js
+++ kk.js
@@ -3,8 +3,8 @@ const app = express();
 const port = 3000; // You can change this to any port number you prefer
 
 // Define a route that returns "Hello, World!"
-app.get('/hh', (req, res) => {
-    res.send('Hello, World!alkkl');
+app.get('/abd', (req, res) => {
+    res.send('abd');
 });
 
 // Start the server and listen on the specified port
